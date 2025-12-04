0cd8e6b3e5517970ee495793f75d18ab3834030c
lequockhanh19521680 <19521680@gm.uit.edu.vn>
2025-12-04 18:30:15 +0700

    init project

---
+++ server/server-user/src/main/java/com/myproject/server_user/ServerUserApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.server_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerUserApplication.class, args);
+	}
+
+}
