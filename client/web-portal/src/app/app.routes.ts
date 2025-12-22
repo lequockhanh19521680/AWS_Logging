7ff5279e5ad7d5a74bff0a07cb79ad422906df5b
lequockhanh19521680 <19521680@gm.uit.edu.vn>
2025-12-22 20:49:24 +0700

    remove, start again

--- client/web-portal/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './auth/login.component';
-import { OtpComponent } from './auth/otp.component';
-
-export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'otp', component: OtpComponent },
-  { path: '', pathMatch: 'full', redirectTo: 'login' }
-];
--- server/identity-service/src/main/java/com/khanh/fintech_backend/application/ports/out/UserRepositoryPort.java
+++
@@ -1,8 +0,0 @@
-package com.khanh.fintech_backend.application.ports.out;
-
-import com.khanh.fintech_backend.domain.model.User;
-
-public interface UserRepositoryPort {
-    User findByUsername(String username);
-    User save(User user);
-}
